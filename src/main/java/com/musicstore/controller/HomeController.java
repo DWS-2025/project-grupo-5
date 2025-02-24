ce3d82995bce6200a9b9a10902242332e81eef1f
paul <pablomartavil@protonmail.com>
2025-02-24 23:49:56 +0100

    only admin can upload, edit and delete albums. admin can delete reviews. profile username can be changed

--- src/main/java/com/musicstore/controller/HomeController.java
+++ src/main/java/com/musicstore/controller/HomeController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -54,6 +56,18 @@ public class HomeController {
         }
         return "redirect:/login";
     }
+
+    @PostMapping("/profile/update")
+    public String profileUpdate(@ModelAttribute User updatedUser, HttpSession session) {
+        User currentUser = (User) session.getAttribute("user");
+        if (currentUser != null) {
+            updatedUser.setId(currentUser.getId());
+            updatedUser.setFavoriteAlbumIds(currentUser.getFavoriteAlbumIds());
+            User updated = userService.updateUser(updatedUser);
+            session.setAttribute("user", updated);
+            return "redirect:/profile";
+        }
+        return "redirect:/login";
+    }
+
 }
-
-
