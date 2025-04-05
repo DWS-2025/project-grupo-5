5e3c70409567e9441c58a217d53fb5b1aacdc9b2
darkxvortex <126343583+darkxvortex@users.noreply.github.com>
2025-04-05 20:11:49 +0200

    Fixed Errors

--- src/main/java/com/musicstore/service/DataLoader.java
+++ src/main/java/com/musicstore/service/DataLoader.java
@@ -1,4 +1,5 @@
-package com.musicstore.service;
+
+/*package com.musicstore.service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -82,3 +83,4 @@ public class DataLoader {
     }
 
 }
+*/
