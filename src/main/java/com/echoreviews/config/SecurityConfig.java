885b608c9c6c1dc49c121dd5317713ca86480628
darkxvortex <126343583+darkxvortex@users.noreply.github.com>
2025-05-18 10:16:51 +0200

    dependencies

--- src/main/java/com/echoreviews/config/SecurityConfig.java
+++ src/main/java/com/echoreviews/config/SecurityConfig.java
@@ -1,10 +1,16 @@
 package com.echoreviews.config;
 
 import com.echoreviews.service.UserService;
+import com.echoreviews.config.CustomAuthenticationSuccessHandler;
+import com.echoreviews.config.BannedUserFilter;
+import com.echoreviews.config.UserAgentValidationFilter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -41,6 +47,11 @@ public class SecurityConfig {
     @Bean
     public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
         return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher());
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
