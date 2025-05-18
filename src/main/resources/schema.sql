5c54904db769e3cd99d221aaf44968fc031cbe44
darkxvortex <126343583+darkxvortex@users.noreply.github.com>
2025-05-18 23:45:24 +0200

    delete innecesary files

--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
--- Script to initialize the MySQL database
-
-CREATE DATABASE IF NOT EXISTS musicstore;
-USE musicstore;
-
--- Character set and collation configuration
-ALTER DATABASE musicstore CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
